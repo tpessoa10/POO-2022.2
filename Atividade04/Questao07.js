6aeb5be34be6629cdc873c2d0b9c10824b2da565
tpessoa10 <48775460+tpessoa10@users.noreply.github.com>
2022-09-19 20:18:09 -0300

    Add files via upload

---
+++ Atividade04/Questao07.js
@@ -0,0 +1,4 @@
+var ely = 'Ely';
+var num = '126.56';
+var typ = 'TypeScript';
+console.log(ely, '\nMay paymant time is', num, '\nand', '\nmy preffered language is', typ);
---
+++ Atividade04/Questao07.ts
@@ -0,0 +1,4 @@
+var ely: string = 'Ely'
+var num: string = '126.56'
+var typ: string = 'TypeScript'
+console.log(ely,'\nMay paymant time is',num,'\nand','\nmy preffered language is',typ)
